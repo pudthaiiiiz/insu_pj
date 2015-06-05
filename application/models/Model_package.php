0623f7f1b5f5420d1e624102a5c483eb9ca83fc1
คุณ ผัดไท <pudthaiiii@gmail.com>
2015-06-06 05:57:03 +0700

    update

--- application/models/Model_package.php
+++ application/models/Model_package.php
@@ -84,7 +84,7 @@ class Model_package extends CI_Model{
     $this->db->where('mYear',$year);
     $this->db->where('mBrand',$brand);
     $this->db->where('mSeries',$series);
-    $this->db->order_by('mName','ASC');
+    $this->db->order_by('mDetail','ASC');
     $query = $this->db->get('tbl_package_main');
     $isData = $query->num_rows();
       if($isData > 0){
