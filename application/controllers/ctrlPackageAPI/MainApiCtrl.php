51458fa64ba8d6b37cb6afa8fcb9745488941349
คุณ ผัดไท <pudthaiiii@gmail.com>
2015-06-06 05:36:59 +0700

    update App

---
+++ application/controllers/ctrlPackageAPI/MainApiCtrl.php
@@ -0,0 +1,52 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class SeriesApiCtrl extends CI_Controller {
+
+  var $timeStamp;
+  var $dateTime;
+  var $cusToker;
+
+  public function __construct() {
+    parent::__construct();
+    $this->load->model('Model_package');
+    $this->timeStamp = time();
+    $this->dateTime = date('Y-m-d H:i:s');
+  }
+
+  public function callSaveService() {
+    $resp['status'] = 'error';
+    $this->load->library('form_validation');
+    $_POST = json_decode(file_get_contents("php://input"), true);
+    $this->form_validation->set_rules('inputSeries', 'fullname', 'required');
+    if ($this->form_validation->run() == FALSE) {
+      $resp['result'] = 'bot';
+    } else {
+      $values = array('sName' => $this->input->post('inputSeries'));
+
+      $isRegister = $this->Model_package->saveSeries($values);
+      if ($isRegister) {
+        $resp['status'] = 'success';
+      }
+    }
+    $result = json_encode($resp);
+    echo $result;
+  }
+
+  
+  public function callGetService(){
+    $resp['status'] = 'error';
+    $resp['data'] = null;
+    
+    $_POST = json_decode(file_get_contents("php://input"), true);
+    
+    $isData = $this->Model_package->getMain($this->input->post('inputYear'),$this->input->post('inputIdBrand'),$this->input->post('inputIdSeries'));
+      if($isData != 0 ){
+        $resp['status'] = 'success';
+        $resp['data'] = $isData;
+      }
+    $result = json_encode($resp);
+    echo $result;
+  }
+}
