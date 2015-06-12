dccfafacb7d9dc9de97fd0e41ebf50025d3bcfbb
Apple Macintosh <Admin@Apple-Macintosh-2.local>
2015-06-13 00:12:55 +0700

    New model content

--- application/controllers/ContentCtrl.php
+++ application/controllers/ContentCtrl.php
@@ -3,24 +3,25 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class ContentCtrl extends CI_Controller {
 
+  public function __construct() {
+    parent::__construct();
+    $this->load->model('Model_content');
+  }
+
   public function index()
 	{
-    $pathAsset = assets();
-    $data = array(
-                  'title' => 'insurancebroker360',
-                  'assets' => $pathAsset
-                );
-    $this->parser->parse('content', $data);
+    return;
 	}
   
   public function detail($id)
-	{
+	{  
+    $result = $this->Model_content->getContent($id);
     $pathAsset = assets();
     $data = array(
                   'title' => 'insurancebroker360',
                   'assets' => $pathAsset,
-                  'contentTitle' => 'title',
-                  'contentDetail' => 'detail'
+                  'contentTitle' => $result['title'],
+                  'contentDetail' => $result['detail']
                 );
     $this->parser->parse('content', $data);
 	}
---
+++ application/models/Model_content.php
@@ -0,0 +1,36 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Model_content extends CI_Model{
+
+	public function __construct(){
+		parent::__construct();
+		
+	}
+  
+  public function getContent($id){
+//    $this->db->order_by('yName','DESC');
+//    $query = $this->db->get('tbl_package_year');
+//    $isData = $query->num_rows();
+//      if($isData > 0){
+//        $result = $query->result();
+//      }
+    $result = array(
+        'title' => 'content id : '.$id,
+        'detail' => 'content detail'
+    );
+    return $result;
+   
+  }
+  
+	
+}
+
+
+
+
+
+
+
+
+
+
+
